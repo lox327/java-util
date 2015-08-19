980c101bc0730e96c56f415fe452da81411c42bd
lox327 <lox327@users.noreply.github.com>
2015-08-19 15:41:27 -0700

    adding static fields/methods

--- src/Timer.java
+++ src/Timer.java
@@ -1,31 +1,28 @@
+
 public class Timer {
 
-	private long startTime;
-	private long endTime;
-	private long duration;
+	static private long startTime;
+	static private long endTime;
+	static private long duration;
 		
-	protected void startTimer() {
+	static protected void startTimer() {
 		startTime = System.nanoTime();
 	}
 	
-	protected void endTimer() {
+	static protected void endTimer() {
 		endTime = System.nanoTime();
 		duration = (endTime - startTime)/1000000; //divide by 1000000 to get milliseconds
 	}
 	
-	protected long getDuration() {
+	static protected long getDuration() {
 		return duration;
 	}
 	
-	protected double getDurationSeconds() {
-		return (double)duration/1000;
-	}
-	
-	protected void printTime() {
+	static protected void printTime() {
 		System.out.println(duration + "ms");
 	}
 	
-	protected void printTimeSeconds() {
+	static protected void printTimeSeconds() {
 		System.out.println((double)duration/1000 + "s");
 	}
 	
