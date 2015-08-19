6a54100b21cb67bdd13aed9a47d58946d0d26707
lox327 <lox327@users.noreply.github.com>
2015-08-19 15:47:34 -0700

    adding doc

--- src/Timer.java
+++ src/Timer.java
@@ -3,16 +3,22 @@ public class Timer {
 	private long startTime;
 	private long endTime;
 	private long duration;
-		
+	
+	/** Start the timer */	
 	protected void startTimer() {
 		startTime = System.nanoTime();
 	}
 	
+	/** Stop the timer */	
 	protected void endTimer() {
 		endTime = System.nanoTime();
 		duration = (endTime - startTime)/1000000; //divide by 1000000 to get milliseconds
 	}
 	
+	/** Returns the duration
+     	* If Value is Zero, Return Zero Value String Such as N/A or Included
+     	* @return duration of cycle time in ms
+     	*/ 
 	protected long getDuration() {
 		return duration;
 	}
