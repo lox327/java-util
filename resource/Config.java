77d05ef4de261ad8f7be2e1731f565fc494b97f7
lox327 <lox327@users.noreply.github.com>
2016-02-05 14:32:27 -0800

    Update and rename Main.java to Config.java

--- resource/Config.java
+++ resource/Config.java
@@ -1,6 +1,6 @@
 import java.io.IOException;
 
-public class Main {
+public class Config {
 
 	public static void main(String[] args) throws IOException {
 		GetPropValues properties = new GetPropValues();
