931ad1d9db75fbddba4749c79d3d846a6e2323e3
lox327 <lox327@users.noreply.github.com>
2016-03-01 15:02:27 -0800

    Update AudioConverter.java
    adding to load from prop

--- src/AudioConverter/AudioConverter.java
+++ src/AudioConverter/AudioConverter.java
@@ -5,18 +5,21 @@ import it.sauronsoftware.jave.EncodingAttributes;
 import it.sauronsoftware.jave.InputFormatException;
 
 import java.io.File;
+import java.io.IOException;
 
-public class AudioConverter {
-	public void convertOne(String source, String target) throws IllegalArgumentException, InputFormatException, EncoderException {
+public class AudioConvert {
+	public void convertOne(String source, String target) throws IllegalArgumentException, InputFormatException, EncoderException, IOException {
+		GetPropValues prop = new GetPropValues();
 		File src = new File(source);
 		File targ = new File(target);
 		AudioAttributes audio = new AudioAttributes();
-		audio.setCodec("libmp3lame");
-		audio.setBitRate(new Integer(128000));
-		audio.setChannels(new Integer(2));
-		audio.setSamplingRate(new Integer(44100));
+		
+		audio.setCodec(prop.getProp("codec"));
+		audio.setBitRate(new Integer(prop.getProp("bitrate")));
+		audio.setChannels(new Integer(prop.getProp("channels")));
+		audio.setSamplingRate(new Integer(prop.getProp("samplerate")));
 		EncodingAttributes attrs = new EncodingAttributes();
-		attrs.setFormat("mp3");
+		attrs.setFormat(prop.getProp("format"));
 		attrs.setAudioAttributes(audio);
 		
 		Encoder encoder = new Encoder();
@@ -24,7 +27,7 @@ public class AudioConverter {
 	}
 	
 
-	public void convertAll(String audioPath) throws IllegalArgumentException, InputFormatException, EncoderException {
+	public void convertAllToMp3(String audioPath) throws IllegalArgumentException, InputFormatException, EncoderException, IOException {
 		File folder = new File(audioPath);
 		File[] listOfFiles = folder.listFiles();
 
@@ -39,5 +42,36 @@ public class AudioConverter {
 	    	}
 	    }
 	}
+	
+	public void convertAllToWav(String audioPath) throws IllegalArgumentException, InputFormatException, EncoderException, IOException {
+		File folder = new File(audioPath);
+		File[] listOfFiles = folder.listFiles();
 
+		String source = null;
+		String target = null;
+		
+	    for (int i = 0; i < listOfFiles.length; i++) {
+	    	if (listOfFiles[i].isFile() && !listOfFiles[i].getName().contains(".wav")) {
+	    		source = audioPath + "\\" + listOfFiles[i].getName();
+	    		target = audioPath + "\\" + listOfFiles[i].getName().replace("mp3", "wav");
+	    		convertOne(source, target);
+	    	}
+	    }
+	}
+	
+	public void convertAll(String audioPath, String targetFormat) throws IllegalArgumentException, InputFormatException, EncoderException, IOException {
+		File folder = new File(audioPath);
+		File[] listOfFiles = folder.listFiles();
+
+		String source = null;
+		String target = null;
+		
+	    for (int i = 0; i < listOfFiles.length; i++) {
+	    	if (listOfFiles[i].isFile() && !listOfFiles[i].getName().contains(targetFormat)) {
+	    		source = audioPath + "\\" + listOfFiles[i].getName();
+	    		target = audioPath + "\\" + listOfFiles[i].getName().replace("wav", targetFormat);
+	    		convertOne(source, target);
+	    	}
+	    }
+	}
 }
